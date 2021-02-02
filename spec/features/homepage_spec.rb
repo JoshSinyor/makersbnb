be97f8c6b91092a20bd7ac24a8fe55b435edfed6
Joshua Sinyor <josh.sinyor@gmail.com>
2021-02-02 11:36:34 +0000

    Updated homepage_spec.rb. This reflects a failing RSpec feature test.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Home page feature test
 feature 'HomePage' do
   scenario 'user visits homepage' do
@@ -12,6 +13,23 @@ feature 'HomePage' do
     fill_in :description, with: 'a space'
     fill_in :price, with: '£1.00'
     click_button :submit
+
     expect(page).to have_content 'space a space £1.00'
+  end
+
+  scenario 'user adds listing' do
+    visit('/')
+    fill_in :name, with: 'space'
+    fill_in :description, with: 'a space'
+    fill_in :price, with: '£1.00'
+    click_button :submit
+
+    fill_in :name, with: 'space_2'
+    fill_in :description, with: 'a space_2'
+    fill_in :price, with: '£2.00'
+    click_button :submit
+
+    expect(page).to have_content 'space a space £1.00'
+    expect(page).to have_content 'space_2 a space_2 £2.00'
   end
 end
