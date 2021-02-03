b7ac6970990f7be2473593c57a9bf6de47e17ace
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-03 14:48:56 +0000

    correct feature test for sign in

--- spec/features/start_session_spec.rb
+++ spec/features/start_session_spec.rb
@@ -5,11 +5,16 @@ feature "starting session" do
   scenario "welcomed by name after session start" do
     visit "/"
     click_button "register"
-
     fill_in :user_name, with: DEFAULT_USER_NAME
     fill_in :user_email, with: DEFAULT_USER_EMAIL
     fill_in :password, with: DEFAULT_USER_PASSWORD
     click_button 'save'
+
+    click_button "Sign in"
+    expect(current_path).to eq "/sign_in"
+    fill_in :user_email, with: DEFAULT_USER_EMAIL
+    fill_in :password, with: DEFAULT_USER_PASSWORD
+    click_button "Sign in"
 
     expect(current_path).to eq "/"
     expect(page).to have_content DEFAULT_USER_NAME
