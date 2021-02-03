379a5d31323f628fa4003ea7c369a829194ab666
Joshua Sinyor <josh.sinyor@gmail.com>
2021-02-03 12:01:20 +0000

    Updated schema.rb and created migration file create_users_table.rb. This reflects creation of the Users table in the database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_102843) do
+ActiveRecord::Schema.define(version: 2021_02_03_114953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2021_02_03_102843) do
     t.string "email"
     t.string "start_date"
     t.string "end_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name", limit: 50
+    t.string "user_email", limit: 50
+    t.string "password_digest"
   end
 
 end
