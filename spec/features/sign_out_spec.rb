69bba6ceaeb520f811a9be8c6cadd0d4b7e1f55e
RTurney <r.turney93@gmail.com>
2021-02-03 16:11:43 +0000

    Add sign out spec and feature

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,15 @@
+feature 'Signing out' do
+  scenario 'a user signs in then out' do
+    register_new_user(DEFAULT_USER_NAME, DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD)
+
+    click_button 'Sign In'
+    expect(current_path).to eq '/sign_in'
+    fill_in :user_email, with: DEFAULT_USER_EMAIL
+    fill_in :password, with: DEFAULT_USER_PASSWORD
+    click_button 'Sign In'
+
+    click_button 'Sign Out'
+    expect(page.status_code).to eq 200
+    expect(page).not_to have_content "Welcome Big #{DEFAULT_USER_NAME[0]}!"
+  end
+end
