ea78690e2611fd0ed77804fa2ae60803ccbe22de
Joshua Sinyor <josh.sinyor@gmail.com>
2021-02-04 16:49:48 +0000

    Add booking push to table.

--- app.rb
+++ app.rb
@@ -4,10 +4,11 @@ require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
-require "sinatra/flash"
+require 'sinatra/flash'
 require 'bcrypt'
 require './lib/space'
 require './lib/user'
+require './lib/booking'
 
 # MakersBNBapp is the controller.
 class MakersBNBapp < Sinatra::Base
@@ -38,7 +39,7 @@ class MakersBNBapp < Sinatra::Base
                       price: params['price'],
                       start_date: params['start_date'],
                       end_date: params['end_date'],
-                    user_id: session[:session_user].id)
+                      user_id: session[:session_user].id)
     space.save
     redirect '/'
   end
@@ -58,6 +59,13 @@ class MakersBNBapp < Sinatra::Base
   post '/listing-:id' do
     session[:booking_requested] = true
     session[:listing_id] = params[:id]
+
+    booking = Booking.new(start_date: params['date'],
+                          space_id: session[:space_id],
+                          user_id: session[:session_user].id,
+                          accepted: false)
+
+    booking.save
     redirect '/listing-:id'
   end
 
